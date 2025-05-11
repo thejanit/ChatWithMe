56c5d40da572562b7145504ef9726618a5b9de7a
thejanit <janitjanit1999@gmail.com>
2025-05-11 18:56:09 +0530

    updated ws to wss for secure access

--- ChatWithMe-Frontend/src/constants.jsx
+++ ChatWithMe-Frontend/src/constants.jsx
@@ -1,6 +1,6 @@
 const baseURL = "https://chatwithme-pyog.onrender.com";
 const SHARED_SECRET = "SecretKey@20!";
-const webSocketUrl = "ws://chatwithme-pyog.onrender.com";
+const webSocketUrl = "wss://chatwithme-pyog.onrender.com";
 
 export default baseURL;
 export { SHARED_SECRET, webSocketUrl };
